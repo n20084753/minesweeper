dbe7012f74ae771bd000f58ac7881a68313472a1
Rajeshkumar Animuthu <n20084753>
2019-07-29 17:42:52 +0530

    Updates

---
+++ precache-manifest.a16e142941abf20aaba634d31cd059e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e0b7cb6be92dd92d65834cc0f1f431",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "97cc882da916859405ab",
+    "url": "/minesweeper/static/css/main.a3bdbafc.chunk.css"
+  },
+  {
+    "revision": "9c63adaad737e3a5be0a",
+    "url": "/minesweeper/static/js/2.9a7e649c.chunk.js"
+  },
+  {
+    "revision": "97cc882da916859405ab",
+    "url": "/minesweeper/static/js/main.42de07ef.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  }
+]);
